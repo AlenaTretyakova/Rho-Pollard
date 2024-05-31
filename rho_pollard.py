2cb09f55aa2f279f48595d7f17fb8dfca34dac5b
АлёнаХудякова <130465240+AlenaTretyakova@users.noreply.github.com>
2024-05-31 23:38:11 +0300

    Add files via upload

---
+++ rho_pollard.py
@@ -0,0 +1,32 @@
+def algorithm():
+    a = 15
+    b = 4
+    p = 31
+    x = 0
+
+    ui = 0
+    vi = 0
+    zi = 0
+    t = True
+
+    table = [[0, 0, 1]]
+
+    while t:
+        if zi * 3 < p:
+            ui += 1
+        elif zi * 3 > 2 * p:
+            vi += 1
+        elif (3 * zi > p) and (3 * zi < 2 * p):
+            ui *= 2
+            vi *= 2
+        zi = ((a ** ui) * (b ** vi)) % p
+        table.append([ui, vi, zi])
+        for i in range(0, len(table) - 2):
+            if table[i][2] == zi:
+                t = False
+                x = ((table[i][0] - ui) / (vi - table[i][1])) % (p - 1)
+    if x < 0:
+        x += p
+    return x
+
+
