d14cade5879c418285eefc6b3fcb8232903294c7
АлёнаХудякова <130465240+AlenaTretyakova@users.noreply.github.com>
2024-06-09 17:58:48 +0300

    Delete test_rho-Pollard.py

--- test_rho-Pollard.py
+++
@@ -1,6 +0,0 @@
-from rho_pollard import algorithm
-def test_algorithm():
-    assert algorithm() == 28, "Should be 28"
-if __name__ == "__main__":
-    test_algorithm()
-    print("Completed successfully")
