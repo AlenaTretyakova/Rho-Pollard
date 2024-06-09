3e557c942f6edf31487b3a7d4159aab9f3566fc2
АлёнаХудякова <130465240+AlenaTretyakova@users.noreply.github.com>
2024-06-09 17:45:30 +0300

    Update rho_pollard.py

--- rho_pollard.py
+++ rho_pollard.py
@@ -1,33 +1,33 @@
-def algorithm():
-    a = 15
-    b = 4
-    p = 31
-    x = 0
 
-    ui = 0
-    vi = 0
-    zi = 0
-    t = True
+a = 15
+b = 4
+p = 31
+x = 0
 
-    table = [[0, 0, 1]]
+ui = 0
+vi = 0
+zi = 0
+t = True
 
-    while t:
-        if zi * 3 < p:
-            ui += 1
-        elif zi * 3 > 2 * p:
-            vi += 1
-        elif (3 * zi > p) and (3 * zi < 2 * p):
-            ui *= 2
-            vi *= 2
-        zi = ((a ** ui) * (b ** vi)) % p
-        table.append([ui, vi, zi])
-        for i in range(0, len(table) - 2):
-            if table[i][2] == zi:
-                t = False
-                x = ((table[i][0] - ui) / (vi - table[i][1])) % (p - 1)
-    if x < 0:
-        x += p
-    return x
+table = [[0, 0, 1]]
 
-def algorithm()
+while t:
+    if zi * 3 < p:
+        ui += 1
+    elif zi * 3 > 2 * p:
+        vi += 1
+    elif (3 * zi > p) and (3 * zi < 2 * p):
+        ui *= 2
+        vi *= 2
+    zi = ((a ** ui) * (b ** vi)) % p
+    table.append([ui, vi, zi])
+    for i in range(0, len(table) - 2):
+        if table[i][2] == zi:
+            t = False
+            x = ((table[i][0] - ui) / (vi - table[i][1])) % (p - 1)
+if x < 0:
+    x += p
+print (x)
+
+
 
